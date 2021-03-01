1d2bf670b10111f1ca873cffb29ef65108047b29
cesarrobertoruizmacias <cesarrobertorm@gmail.com>
2021-03-01 01:14:46 -0600

    add files for linkedList

---
+++ src/tarea45/Main.java
@@ -0,0 +1,12 @@
+package tarea45;
+
+import tarea45.list.linkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        linkedList lista = new linkedList()
+    }
+
+}
---
+++ src/tarea45/list/linkedList.java
@@ -0,0 +1,4 @@
+package tarea45.list;
+
+public class linkedList {
+}
